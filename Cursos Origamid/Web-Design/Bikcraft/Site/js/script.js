029d3e47935264dddbe23100986cbc26e72314d1
Bruno Cheles <bruno.cheles@outlook.com>
2021-07-05 16:38:34 -0300

    Início JS#1

---
+++ Cursos Origamid/Web-Design/Bikcraft/Site/js/script.js
@@ -0,0 +1,15 @@
+if (window.SimpleSlide) {
+    new SimpleSlide ({
+        slide: "slider",
+        time: 5000
+    });
+    new SimpleSlide ({
+        slide: "portf",
+        time: 5000,
+        nav: true
+    });
+}
+
+if (window.SimpleAnime) {
+    new SimpleAnime();
+}
